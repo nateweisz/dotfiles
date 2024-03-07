61be4a72bc7807652e02d6a90685f010272d4904
= <nathan@weisz.org>
2024-03-06 21:28:16 -0800

    Stuff

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+killall -q i3bar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar main &
+
+echo "Polybar launched..."
