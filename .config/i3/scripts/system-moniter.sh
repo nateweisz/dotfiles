6d43a6f97ab70849d8d8d17cc4f61e81fa510455
= <nathan@weisz.org>
2024-03-13 12:31:27 -0700

    feed

--- .config/i3/scripts/system-moniter.sh
+++ .config/i3/scripts/system-moniter.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 i3-msg "workspace 10; append_layout /home/nopox/dotfiles/.config/i3/workspaces/workspace-10.json"
-sleep 1s
+sleep 0.25s
 i3-msg 'workspace 10; exec alacritty -e sh -c "/home/nopox/.cargo/bin/netscanner"'
-sleep 1s
+sleep 0.25s
 i3-msg 'workspace 10; exec alacritty -e sh -c "ctop"'
-sleep 1s
+sleep 0.25s
 i3-msg 'workspace 10; exec alacritty -e sh -c "btm"'
+sleep 0.25s
+i3-msg 'workspace 7; exec alacritty -e sh -c "russ read -d /home/nopox/.config/russ/feeds.db"'
