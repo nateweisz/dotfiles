aedfa1903db44b64d52ce745e0668edbf6b4ac68
Nathan Weisz <nathan@weisz.org>
2024-03-19 21:20:42 -0700

    fixed workspaces

--- .config/i3/scripts/system-moniter.sh
+++ .config/i3/scripts/system-moniter.sh
@@ -11,3 +11,4 @@ sleep 0.25s
 i3-msg "workspace 7; append_layout /home/nopox/.config/i3/workspaces/workspace-7.json"
 sleep 0.25s
 i3-msg 'workspace 7; exec alacritty -e sh -c "/home/nopox/.cargo/bin/russ read -d /home/nopox/.config/russ/feeds.db"'
+sleep 0.25s
